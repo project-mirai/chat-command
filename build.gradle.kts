64c265425f1a78e91bb60d1b509465e20dbb3b08
Him188 <Him188@mamoe.net>
2020-12-19 01:12:53 +0800

    Update to mirai-core 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.0-M1-dev-2"
+    id("net.mamoe.mirai-console") version "2.0-M1"
 }
 
 group = "net.mamoe"
