8aae12e570a5b6997bebf291111b614ae0de6c99
xiaoyv404 <xiaoyv_404@outlook.com>
2022-06-11 11:33:13 +0800

    Use mirai 2.11.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.0-RC"
+    id("net.mamoe.mirai-console") version "2.11.1"
 }
 
 group = "net.mamoe"
